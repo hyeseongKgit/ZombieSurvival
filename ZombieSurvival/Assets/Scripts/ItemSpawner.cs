bbc787756fac023c7e5b31c7867789b90b79b17b
hyeseongKgit <hsk8256@naver.com>
2025-04-03 18:42:11 +0900

    아이템스포너 아이템인터페이스 대리자 고민중

--- ZombieSurvival/Assets/Scripts/ItemSpawner.cs
+++ ZombieSurvival/Assets/Scripts/ItemSpawner.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 
 public class ItemSpawner : MonoBehaviour
 {
+    [SerializeField]
+    List<GameObject> itemPrefabs;
+
+    List<IItem> currentItems;
     public void Init()
     {
+        currentItems = new List<IItem>();
+        StartCoroutine(SpawnItem());
+    }
 
+    IEnumerator SpawnItem()
+    {
+        while(true)
+        {
+            //맵에 아이템이 3개 이상 생성되지 않게 제어
+            if (currentItems.Count < 3)
+            {
+                var item = Instantiate(itemPrefabs[Random.Range(0, itemPrefabs.Count)]).GetComponent<IItem>();
+                //여기 인터페이스에 대리자 어쩌구 구조고민하고있었음
+                item.Remove += () => { currentItems.Remove(item); Debug.Log($"지금 아이템 개수 : {currentItems.Count}"); };
+                currentItems.Add(item);
+                Debug.Log($"지금 아이템 개수 : {currentItems.Count}");
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
